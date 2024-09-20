2a3904283255928a8bbd663bcb22d235a8328d81
60138546 <1011888891@qq.com>
2024-09-20 13:37:29 +0800

    改成相对路径

--- index.node.js
+++ index.node.js
@@ -1,22 +1,24 @@
-// index.node.js
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
+
+// 获取当前文件的目录路径
+const __dirname = new URL('.', import.meta.url).pathname;
 
 const poetsPath = path.join(__dirname, 'source', 'poets');
 const poemsPath = path.join(__dirname, 'source', 'poems');
 
 export async function getPoets() {
-    const files = await fs.promises.readdir(poetsPath);
+    const files = await fs.readdir(poetsPath);
     return Promise.all(files.map(async (file) => {
-        const content = await fs.promises.readFile(path.join(poetsPath, file), 'utf-8');
+        const content = await fs.readFile(path.join(poetsPath, file), 'utf-8');
         return JSON.parse(content);
     }));
 }
 
 export async function getPoems() {
-    const files = await fs.promises.readdir(poemsPath);
+    const files = await fs.readdir(poemsPath);
     return Promise.all(files.map(async (file) => {
-        const content = await fs.promises.readFile(path.join(poemsPath, file), 'utf-8');
+        const content = await fs.readFile(path.join(poemsPath, file), 'utf-8');
         return JSON.parse(content);
     }));
 }
